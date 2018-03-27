4dee323a02e59399564ec6bfca183e8c31a61d61
Dan Kempton <kemptondan@gmail.com>
2018-03-27 16:15:55 -0400

    updated displayTodos

--- app/app.js
+++ app/app.js
@@ -6,28 +6,48 @@ var todoList = {
 	// list
 	todos: [],
 
+	// display todos
+	displayTodos: function() {
+    if (this.todos.length === 0) {
+      console.log('Your todo list is empty!');
+    } else {
+      console.log('My todos');
+      for (var i = 0; i < this.todos.length; i++) {
+        if (this.todos[i].completed === true) {
+          console.log('(x)', this.todos[i].todoText);
+        } else {
+          console.log('()', this.todos[i].todoText);
+        }
+      }
+    }
+  },
+
 	// addTodos - adding todo objects with text description and completed property
 	addTodo: function(todoText) {
 		this.todos.push({
 			todoText: todoText,
 			completed: false
 		});
+		todoList.displayTodos();
 	},
 
 	// updateTodo - change todos that were previously added
 	updateTodo: function(position, todoText) {
 		this.todos[position].todoText = todoText;
+		todoList.displayTodos();
 	},
 
 	// deleteTodo - delete todos that have been added
 	deleteTodo: function(position) {
 		this.todos.splice(position, 1);
+		todoList.displayTodos();
 	},
 
 	// mark completed todo items
 	markCompleted: function(position) {
 		var todo = this.todos[position];
 		todo.completed = !todo.completed; // flipping completed property from false to true
+		todoList.displayTodos();
 	},
 
 	// toggle all items - off an on
@@ -49,10 +69,17 @@ var todoList = {
 				todo.completed = true;
 			}
 		});
+		todoList.displayTodos();
 	}
 };
 
 
 // should have an object that stores handlers - inputs and buttons
-
+var handlers = {
+	addTodo: function() {
+		var addTodoTextInput = document.getElementById('addTodoTextInput');
+		todoList.addTodo(addTodoTextInput.value);
+		addTodoTextInput.value = '';
+	}
+};
 // should have an object for the view / display of the list 
